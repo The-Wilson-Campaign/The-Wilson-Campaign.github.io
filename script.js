b01882ce28ee8767b643ab44d1ecf11741d9583d
TheWilsonCampaign <128070574+TheWilsonCampaign@users.noreply.github.com>
2023-03-21 11:11:01 -0400

    Updated bg music

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
+function Sound(source, volume, loop)
+{
+    this.source = source;
+    this.volume = volume;
+    this.loop = loop;
+    var son;
+    this.son = son;
+    this.finish = false;
+    this.stop = function()
+    {
+        document.body.removeChild(this.son);
+    }
+    this.start = function()
+    {
+        if (this.finish) return false;
+        this.son = document.createElement("embed");
+        this.son.setAttribute("src", this.source);
+        this.son.setAttribute("hidden", "true");
+        this.son.setAttribute("volume", this.volume);
+        this.son.setAttribute("autostart", "true");
+        this.son.setAttribute("loop", this.loop);
+        document.body.appendChild(this.son);
+    }
+    this.remove = function()
+    {
+        document.body.removeChild(this.son);
+        this.finish = true;
+    }
+    this.init = function(volume, loop)
+    {
+        this.finish = false;
+        this.volume = volume;
+        this.loop = loop;
+    }
+}
+
 window.onload() {
-  new Audio('1-07 Goat Storm(Nyan Goat Mix).mp3').play();
+  var goat = new Sound('1-07 Goat Storm(Nyan Goat Mix).mp3', 100, true);
+  goat.start();
 }
